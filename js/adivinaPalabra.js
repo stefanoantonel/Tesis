8a04e59652e02c4a3a38003367789dda88a86ca9
stefanoantonel <stefanoantonel@gmail.com>
2014-08-10 13:12:04 -0300

    limpieza de codigo

--- js/adivinaPalabra.js
+++ js/adivinaPalabra.js
@@ -5,13 +5,11 @@ function readyOk(idObj,idBox,idBox2){
 	temp1=document.getElementById(idObj);
 	boxes=[idBox,idBox2];
 	temp=$(temp1);
-	//loadTxt(boxes,temp);
 	var conf;
 	conf=getConfig(2,callback);
 }
 
 function callback(data){
-	
 	var selectedText=getTextRand(data);
 	var parts=divide(selectedText);
 	numParts=$(parts).size();
@@ -29,27 +27,9 @@ function functionsDD(context,currElem){
 }
 
 var numParts=0;
-/*
-function loadTxt(boxes,temp){
-	$.ajax({
-        url : "test.txt",
-        dataType: "text",
-        async: false,
-        success : function (data) {
-        	var selectedText=getTextRand(data);
-        	var parts=divide(selectedText);
-        	numParts=$(parts).size();
-   
-        	fillTemplate(boxes,temp,parts);
-        	dragAndDrop(parts,boxes); 
-        }
-    });
-}
-*/
 
 function divide(data){
     //divido en partes segun los espacios
-
     var parts=data.split(" ");
     numParts=$(parts).size();
     return parts;
@@ -57,19 +37,14 @@ function divide(data){
 
 function checkCorrect(container) {
 	if($(container).attr('id')=='rightbox'){
-	
 		parts=$(container).children();
-		
 		mal=0;
 		for(var i = 0; i < $(parts).size(); i++){ 
-			//console.log('part id',parseInt(parts[i].id),'   i',i);
 			if (parseInt(parts[i].id) != i){
-				//console.log($('#'+parts[i].id));
 				$('#'+parts[i].id).addClass('errorLetra');
 				mal=1;
 			}
 		}
-		
 		if(parts.size()==numParts && mal==0){ //finalizo y correcto 
 			return true;
 		}
@@ -82,15 +57,11 @@ function checkCorrect(container) {
 }
 
 function getTextRand(data){
-	
-    //var lines=data.split(/\r?\n/);
-    //var quantLines=$(lines).size();
-    var quantLines=$(data).size();
+	var quantLines=$(data).size();
     //saco un numero para decirle el indice del la palabra
     var numberRand = Math.floor(Math.random() * quantLines);
     return data[numberRand];
 }
-
 
 function fillTemplate(boxes,temp, parts){
 	//relleno el template
@@ -102,27 +73,15 @@ function fillTemplate(boxes,temp, parts){
         $(t).attr('id',index);
         $(t).html(part);
         $(t).prop("hidden",false);
-        //console.log(t);
-        //a.push(t);
         a.push(t);
-        //dragAndDrop(index,"leftbox","rightbox");
     });
     disorder(a);
     $(fromBox).append(a);
-   
-}
-function isFinished(parts){
-	//console.log('isFinish');
-	//console.log('es igual',parts.size()==numParts);
-	if (parts.size()==numParts){
-		//console.log('true');
-		return true;
-	}
-	return false;	
-	
 }
 
-function cartelFelicitaciones(){
-	$('article').html('');
-	$('#alertOk').attr('hidden',false);
+function isFinished(parts){	
+	if (parts.size()==numParts){
+		return true;
+	}
+	return false;
 }
--- js/codigo.js
+++ js/codigo.js
@@ -1,70 +1,21 @@
-
-function disorder(o){ //in: list of numbers out: unorder list 
-	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	return o;
-};
-
-
-/* El viejo......antes de pasarle una funcion
-function dragAndDrop(idImg,idBoxes) {
-
-	$(idImg).each(function(ind,part){
-	
-		$('#'+ind).draggable({
-			revert:true,
-		});
-	});
-
-    $(idBoxes).each(function(ind,box){
-    	
-    	$( '#'+box ).droppable({
-        	drop: function( event, ui ) {
-        		$(ui.draggable).css({top:0,left:0});
-        		$( this )
-        			.addClass( "ui-state-highlight" )
-        			.append(ui.draggable);  
-        		//console.log('drop..',ui.draggable);
-        		
-        		//isCorrect=checkCorrect($(this));
-        		//if (isCorrect==true){cartelFelicitaciones();}
-        		
-        	}
-        });
-    });
-    
-}
-*/
 function dragAndDrop(idImg,idBoxes,functions) {
-
-	//console.log('DragDrop');
 	$(idImg).each(function(ind,part){
-		
 		$(this).draggable({
 			revert:true,
 		});
 	});
 
     $(idBoxes).each(function(ind,box){
-    	console.log(this);
-    	//$( '#'+box ).droppable({
     	$( this ).droppable({
         	drop: function( event, ui ) {
         		$(ui.draggable).css({top:0,left:0});
         		$( this )
         			.addClass( "ui-state-highlight" )
         			.append(ui.draggable);  
-        		//console.log('drop..',ui.draggable);
-        		//checkCorrect($( this ).children());
-        		
-        		//checkReplace($(this),ui.draggable);
-        		//checkCorrect(ui.draggable);
-
-        		functions(this,ui.draggable);
-        		
+        		functions(this,ui.draggable); 		
         	}
         });
     });
-    
 }
 
 function getConfig(numAct,callBack){
@@ -73,42 +24,12 @@ function getConfig(numAct,callBack){
 	    }).done(function (){callBack(c);});
 }
 
-function cartelFelicitaciones(){
-	$('article').delay( 800 ).fadeOut( 0 );
-	$('#alertOk').delay( 800 ).fadeIn( 400 );
-	//$('#alertOk').attr('hidden',false);
-}
-
-
-
-/*
-function dragAndDrop1(idImg,idBox,idBox2){
-	
-	pic=document.getElementById(idImg);
-	
-	$(pic).attr('draggable', true);
-	
-	left=document.getElementById(idBox);
-	right=document.getElementById(idBox2);
-	
-	left.addEventListener("dragover",function(e){e.preventDefault()},false); 
-	right.addEventListener("dragover",function(e){e.preventDefault()},false);
-	
-	left.addEventListener("drop",dropped,false);
-	right.addEventListener("drop",dropped,false);
-}
-
-function dropped(e,ui){
-	console.log("dropped");
-	
-	e.preventDefault();
-	this.insertBefore(pic);
-	
-}
-*/
-/*
-function disorderS(o){ //in: list of numbers out: unorder list 
+function disorder(o){ //in: list of numbers out: unorder list 
 	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	return ;
+	return o;
 };
-*/
+
+function cartelFelicitaciones(){
+	$('article').delay( 600 ).fadeOut( 0 );
+	$('#alertOk').delay( 800 ).fadeIn( 400 );
+}
