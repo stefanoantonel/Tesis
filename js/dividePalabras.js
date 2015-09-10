04da5c5c29abb86a5ce863aa1711b6b7a965a1bc
stefanoantonel <stefanoantonel@gmail.com>
2015-09-10 20:37:29 -0300

    fixes in  Divide Palabras

--- js/dividePalabras.js
+++ js/dividePalabras.js
@@ -7,20 +7,10 @@ function functionInit() {
 	getConfigByElement("sentence",level,1,functionCallback);
 	rightBoxTemp = $('#rightboxTemp');
 	rightContainer = $('#rightContainer');
+	$("#finish").click(checkCorrect)
 }
 
 function functionCallback(conf){
-	/*letter = conf[0];
-	getConfigByElementWithFirstLetter("distractors","words",3,functInitWords,letter);*/
-	functInitWords(conf);
-}
-
-function functionsDD(context,currElem){
-	isCorrect=checkCorrect(currElem);
-	if (isCorrect==true) { sessionCounter(); }
-}
-
-function functInitWords(conf){
 	var originalArray = conf[0]["values"];
 	var numParts = originalArray.length;
 	var numSpaces = numParts - 1;
@@ -59,42 +49,26 @@ function functInitWords(conf){
 	$( rightContainer ).disableSelection();
 }
 
-function firstImg(conf){
-	//-------------------------show image
-	t1=$('#rightboxTemp').clone();
-	$(t1).attr('id','firstImage');
-	$(t1).addClass('firstImage');
-	$(t1).attr('src','images/activities/' + conf + '.jpg');	
-	$(t1).removeAttr('hidden');
-	$("#leftContainer").append(t1);
-}
-
-function checkCorrect(part) {
-	var name = $(part).attr("name");
-	if(name == letter) {
-		window.setTimeout(function(){$(part).addClass("animateToFront");},0);
-		window.setTimeout(function(){
-			$(part).addClass("deleted");
-			$("#counterLeft").html(countSameLetters);
-		},700);
-		countSameLetters = countSameLetters - 1;
-		if (countSameLetters == 0) {
-			window.setTimeout(sessionCounter(), 2000);
-		}
-
+function checkCorrect() {
+	var textInContainer = $(rightContainer).text();
+	if(textInContainer == finalSentence) {
+		window.setTimeout(sessionCounter(), 2000);
 		return true;
 	}
 	else {
-		$(".img").css("pointer-events", "none");
+		/*$(".img").css("pointer-events", "none");*/
 		playSound("wrong");
-		$(part).removeClass('normal');
+		changeScore(-10);
+		return false;
+
+		/*$(part).removeClass('normal');
 		$(part).addClass('wrong');
 		window.setTimeout(function() {
 			$(part).removeClass('wrong');
 			$(part).addClass('normal');
 			changeScore(-10);
 			$(".img").css("pointer-events", "auto");
-		}, 500);
-		return false;
+		}, 500);*/
+		
 	}
 }
