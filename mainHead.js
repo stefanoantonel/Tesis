405aa213750e87212d56a1ef60f910e956dd4a94
stefanoantonel <stefanoantonel@gmail.com>
2015-09-30 10:03:49 -0300

    using libraries to load js and css

---
+++ mainHead.js
@@ -0,0 +1,18 @@
+head.load(	
+	"comun/jquery.js",
+	"js/codigo.js",
+	"js/adivinaPalabra.js",
+	"js/loadCSS.js",
+	"js/onloadCSS.js",
+	"js/bootstrap.min.js",
+	"comun/comun.js",
+	"js/jquery-ui.min.js",
+	"js/jquery.ui.touch-punch.min.js",
+	"js/howler.min.js"
+	);
+
+head.ready(function () {
+	loadCSS( "css/bootstrap.min.css" );
+	loadCSS( "css/style.css" );
+	loadCSS( "comun/comun.css" );
+});
