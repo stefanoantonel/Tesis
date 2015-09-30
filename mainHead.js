5f009782d65327983aa9c5beb8998057866bfae8
stefanoantonel <stefanoantonel@gmail.com>
2015-09-30 12:08:43 -0300

    loading js from each activity automatically

--- mainHead.js
+++ mainHead.js
@@ -1,24 +1,23 @@
 document.getElementsByTagName( "html" )[0].className = "loading" ;
 
 head.load(
+	"js/" + location.pathname.substring(location.pathname.lastIndexOf("/") + 1).replace(".html",".js"),
 	"css/bootstrap.min.css",
 	"css/style.css",
 	"comun/comun.css",
 	"comun/jquery.js",
 	"js/codigo.js",
-	"js/adivinaPalabra.js",
 	"js/bootstrap.min.js",
 	"comun/comun.js",
 	"js/jquery-ui.min.js",
 	"js/jquery.ui.touch-punch.min.js",
 	"js/howler.min.js"
+	
 	);
 
 head.ready(function () {
 	waitInterval(200).then(function() {
 		$( "html" ).removeClass( "loading" );	
-	})
-	
-	//document.getElementsByTagName( "html" )[0].className.replace( /loading/, "" );
+	});
 });
 
